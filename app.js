cec7451bea00cc0f90b14b87ff1be79a2ecf2f98
QuangTuyen <quangtuyen1993@gmail.com>
2020-04-24 16:37:39 +0700

    update db

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var UserModel=require("./model/UserModel")
+
+require('./db')
 
 // var indexRouter = require("./routes/index");
 // var usersRouter = require("./routes/users");
@@ -11,9 +14,10 @@ var app = express();
 // app.use(function (req, res, next) {
 //   next(createError(404));
 // });
-app.get("/",(req,res,next)=>{
+app.get("/",async(req,res,next)=>{
+    var response=await UserModel.find()
     res.status(200).send({
-        message:"hello word"
+        message:response
     })
 })
 var port=3000
